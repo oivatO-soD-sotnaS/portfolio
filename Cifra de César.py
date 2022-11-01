4470e98b2192d5c1c20d54a93841e9d88550fb9a
oivatO-soD-sotnaS <116353611+oivatO-soD-sotnaS@users.noreply.github.com>
2022-10-31 21:15:38 -0300

    Update Cifra de César.py

--- Cifra de César.py
+++ Cifra de César.py
@@ -3,6 +3,7 @@ import string
 alphabet = list(string.ascii_lowercase)
 
 
+
 def cesar_info():
     frase = input("Digite a frase à ser encriptada/descriptada:\n")
     chave = int(input("Digite a chave:\n"))
@@ -26,7 +27,12 @@ def descriptacao():
     new_frase = []
     frase = frase.replace(" ","")
     for i in frase:
-        new_frase.append(alphabet[alphabet.index(i)-chave])
+        if alphabet[::-1].index(i)+chave > 25:
+            y = (alphabet[::-1].index(i)+chave)//26
+            x = alphabet[::-1].index(i)+chave
+            new_frase.append(alphabet[::-1][x-y*26])
+        else:
+            new_frase.append(alphabet[::-1][alphabet.index(i)+chave])
     print("".join(new_frase))
     interface()
     
@@ -34,6 +40,9 @@ def interface():
     ans = input("Encriptar[1]\tDescriptar[2]\nResposta: ")
     if ans == "1":
         encriptacao()
-    else:
+    elif ans == "2":
         descriptacao()
+    else:
+        print("Opcao nao encontrada.")
+        interface()
 interface()
